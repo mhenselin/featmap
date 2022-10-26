ef044fa74c4feedd7df09bfe8827e2ed28f9fa36
Eric Zieger <eric.zieger@camao.one>
2022-10-26 17:37:21 +0200

    feat: focus indicator on header link

--- webapp/src/components/Container.tsx
+++ webapp/src/components/Container.tsx
@@ -13,7 +13,7 @@ export const Container: React.FunctionComponent<Readonly<ContainerProps>> = (
   return createElement(
     as,
     {
-      className: `container mx-auto px-4 py-6 md:px-6 md:py-8 lg:px-8 lg:py-10 ${
+      className: `relative container mx-auto px-4 py-6 md:px-6 md:py-8 lg:px-8 lg:py-10 ${
         small ? "max-w-md" : ""
       } ${className ?? ""}`,
     },
--- webapp/src/components/NewHeader.tsx
+++ webapp/src/components/NewHeader.tsx
@@ -2,9 +2,15 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="bg-indigo-700 p-4 text-white md:p-6 lg:px-8">
-      <Link to="/" className="flex items-center gap-2">
-        <img width="32" height="32" src="/apple-touch-icon.png" alt="" />
+    <header className="flex items-start bg-indigo-700 p-2 text-white md:p-4 lg:px-6">
+      <Link to="/" className="focus-inverted flex items-center gap-2 p-2">
+        <img
+          width="32"
+          height="32"
+          src="/apple-touch-icon.png"
+          alt=""
+          className="block"
+        />
         Featmap
       </Link>
     </header>
